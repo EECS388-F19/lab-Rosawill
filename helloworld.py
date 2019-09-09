23e911581b70c02d254c95a0f6334a2116c576d1
rosawill <rosawill@umich.edu>
2019-09-09 17:41:51 -0400

    ugh

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+from random import randint
+
+a = randint(1,100)
+b = randint(1,100)
+
+result = a + b
+
+print("Rosemary")
+print(a)
+print(b)
+print("Sum = " + str(a + b))
+print("Average = "  + str((a + b) / 2))
